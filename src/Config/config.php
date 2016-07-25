774e4431c1e3e00b8cec257c8d732ed7b38dca53
Steve Bauman <steven_bauman@outlook.com>
2016-07-25 17:00:18 -0400

    Added config docs.

--- src/Config/config.php
+++ src/Config/config.php
@@ -2,6 +2,28 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Template
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to configure the template
+    | view to use for rendering HTML tables.
+    |
+    */
+
     'template' => 'table::template',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Empty
+    |--------------------------------------------------------------------------
+    |
+    | This option allows you to configure the message to
+    | display when no records are presented.
+    |
+    */
+
+    'empty' => 'There are no records to display.',
 
 ];
